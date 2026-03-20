d6c7c276e84af68c6777f74a3c0694db78df362a
Pranshu Patel <pranshu.patel@simformsolutions.com>
2026-03-20 15:13:27 +0530

    feat: add husky, prettier, eslint, commmitlint and lint staged

---
+++ commitlint.config.mjs
@@ -0,0 +1,33 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation only changes
+        "style", // Formatting, missing semi-colons, etc (no logic change)
+        "refactor", // Code change that neither fixes a bug nor adds a feature
+        "perf", // Performance improvement
+        "test", // Adding or updating tests
+        "build", // Changes to build system or external dependencies
+        "ci", // CI configuration changes
+        "chore", // Other changes that don't modify src or test files
+        "revert", // Revert a previous commit
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 100],
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+  },
+};
+
+export default config;
